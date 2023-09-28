65a7b374525567a4b5caf71e87a5a2a3952650d7
Nomad <ecaterinamihai97@gmail.com>
2023-09-28 19:49:37 +0300

    "second commit"

--- index.js
+++ index.js
@@ -12,22 +12,3 @@ currentCityTag.innerHTML = currentCity;
 displayCurrentWeather(currentCity);
 
 getForecastFor5Days(currentCity);
-
-
-var scrollToTopButton = document.querySelector(".scroll-to-top");
-
-scrollToTopButton.addEventListener("click", function(){
-    window.scrollTo({
-        top: 0,
-        behavior: "smooth"
-    })
-})
-
-window.addEventListener("scroll", function(){
-    if(window.scrollY > ( window.innerHeight / 2 )){
-        scrollToTopButton.style.visibility = "visible";
-        return;
-    }
-
-    this.scrollToTopButton.style.visibility = "hidden";
-})
